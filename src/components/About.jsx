ee3e740d2bfc72f9e185a9d031ee567ef1de00d0
kandelsatish <satishkandel98@gmail.com>
2021-07-02 22:53:58 +0545

    media-query

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,10 @@ export default function About() {
                     <div className="image"></div>
                 </div>
                 <div className="right">
-                    <h1>hello! This is me satish kandel</h1>
+                    <h1>Hello! This is me satish kandel</h1>
                     <h1>About me</h1>
                     <p>
-                        I’m writing to express my interest in securing the position of React Native Developer. This exciting opportunity appears to be a wonderful fit for my passion and field of studies. I have been learning Mobile Development with React Native and searching for an opportunity to work in a reputed IT company. I'm a beginner and I want to join your company if possible. I believe my academic background and knowledge are the best fit for the position I'm applying for.
+                        I have been working as a React and React native developer since 2019. This exciting opportunity appears to be a wonderful fit for my passion and field of studies.  I'm a beginner and I want to learn everything possible.
                         <br />
                         <br />
                         Talking about my experience, I worked as a Technical Support Representative at IMS Himalayan Shangrila Pvt. Ltd-one of the leading software companies in Nepal. I gained Solid Technical Skills and working habits from there. I worked there for more than a year. My job was to set up database servers, software setups, provide clients training, and provide technical support in case they needed it.
